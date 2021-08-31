608b05f25a0437697add42c757c8b507cc51f62a
dhuggins221 <dhuggins221@gmail.com>
2021-08-31 15:12:29 -0500

    2.0

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-$(document).ready(function () {
-    $("saveBtn").click(function () {
-        var value = $(this)
-        var time = $(this)
+var tasks = {};
 
-        localStorage.setItem (time, value);
+var getTasks = function () {
+  tasks = localStorage.getItem("tasks")
+  if (!savedTasks) {
+    tasks = $('textarea')
+  }
+  tasks= JSON.parse(savedTasks)
+};
 
-        function updateTime() {
-            // get current time
-            var currentTime = moment().hours();
-        
-            // loop over time blocks
-            $('.time-block').each(function() {
-              var timeBlock = parseInt(
-                $(this)
-                  .split('-')[1]
-              )
-
-              if (timeBlock < currentTime) {
-                $(this).addClass('past');
-              } else if (timeBlock === currentTime) {
-                $(this).removeClass('past');
-                $(this).addClass('present');
-              } else {
-                $(this).removeClass('past');
-                $(this).removeClass('present');
-                $(this).addClass('future');
-              }
-            });
-          }
-        
-          updateTime();
-
-          // load values from local storage
-          $('#hour-9 ').val(localStorage.getItem(''));
-          $('#hour-10 ').val(localStorage.getItem(''));
-          $('#hour-11 ').val(localStorage.getItem(''));
-          $('#hour-12 ').val(localStorage.getItem(''));
-          $('#hour-13 ').val(localStorage.getItem(''));
-          $('#hour-14 ').val(localStorage.getItem(''));
-          $('#hour-15 ').val(localStorage.getItem(''));
-          $('#hour-16 ').val(localStorage.getItem(''));
-          $('#hour-17 ').val(localStorage.getItem(''));
-  
-  });
-});
-;
-
+var saveTask = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
